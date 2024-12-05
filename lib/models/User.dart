a417a252c1370a69960a4e614cfbce96ee77a2c7
yatokawai <yatokawai@outlook.com>
2024-12-05 18:19:47 +0000

    correcion de hash

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,18 +1,6 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
-
 class User {
   final String firstName;
   final String lastName;
-  final String encryptedLastName;
-//genera un hash al apellido con el metodo estatico
-  User({required this.firstName, required this.lastName})
-      : encryptedLastName = _generateHash(lastName);
 
-  // Método estático para generar el hash del apellido
-  static String _generateHash(String lastNameSystem) {
-    final bytes = utf8.encode(lastNameSystem); // Convertir el valor a bytes
-    final digest = sha512.convert(bytes); // Generar el hash SHA-512
-    return digest.toString(); // Retornar el hash como String
-  }
+  User({required this.firstName, required this.lastName});
 }
