f37e7c02c8ea7d53285f4e0a78d1fc080ba81c73
AugustoUCE <149543745+AugustoUCE@users.noreply.github.com>
2024-12-10 12:16:05 -0500

    Delete TAREA03-U02-G03/tarea03_u02_g03 directory

--- TAREA03-U02-G03/tarea03_u02_g03/android/app/src/main/kotlin/com/example/tarea03_u02_g03/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tarea03_u02_g03
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- TAREA03-U02-G03/tarea03_u02_g03/lib/models/User.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  final String firstName;
-  final String lastName;
-
-  User({required this.firstName, required this.lastName});
-}
